22b9e9e870bb0afa77d961fce461f712f044dccc
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2021-11-21 16:44:34 +0300

    Move meta too include

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
 document.addEventListener('click', (evt) => {
     const { target } = evt;
-    if (target && target.classList.contains('toggle-items')) {
+    if (!target)
+        return;
+
+    if (target instanceof HTMLElement && target.classList.contains('toggle-items')) {
         let parent = target.parentElement;
-        while (!parent.classList.contains('category') || !parent.parentElement) {
+        while (parent && (!parent.classList.contains('category') || !parent.parentElement)) {
            parent = parent.parentElement;
         }
 
         if (parent) parent.classList.toggle('close');
     }
-})
+});
+
+const burger = document.querySelector('.header__burger');
+if (burger)
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('open');
+    })
+
+
