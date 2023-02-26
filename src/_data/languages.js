9b0b12662d3a730dc9182279c8405341fa9a308b
Илья Космачев <enotsurreal@yandex.ru>
2023-02-27 00:42:32 +0300

    localization menu

---
+++ src/_data/languages.js
@@ -0,0 +1,24 @@
+module.exports = {
+  en: {
+    theme: 'Dark theme',
+    about: 'About',
+    menu: 'Menu',
+    sections: 'Sections',
+    pourover: 'Pourover / V60',
+    aeropress: 'Aeropress',
+    cezve: 'Cezve',
+    coldbrew: 'Cold brew',
+    chemex: 'Chemex'
+  },
+  ru: {
+    theme: 'Темная тема',
+    about: 'О проекте',
+    menu: 'Меню',
+    sections: 'Разделы',
+    pourover: 'Пуровер / V60',
+    aeropress: 'Аэропресс',
+    cezve: 'Джезва/Турка',
+    coldbrew: 'Cold brew',
+    chemex: 'Кемекс'
+  },
+};
