f1558ca68f221de7c67db8babca47d3ce8c6db36
Илья Космачев <enotsurreal@yandex.ru>
2023-02-27 01:21:34 +0300

    localization about page

--- src/_data/languages.js
+++ src/_data/languages.js
@@ -9,7 +9,13 @@ module.exports = {
     cezve: 'Cezve',
     coldbrew: 'Cold brew',
     chemex: 'Chemex',
-    rollup: 'roll up section'
+    rollup: 'roll up section',
+    page_about: {
+      title: 'Try brew - open source project created by coffee lovers to get new taste emotions.',
+      works_on: 'Works on',
+      source_code: 'Source code',
+      suggestions: 'Wishes and suggestions can be sent to:',
+    }
   },
   ru: {
     theme: 'Темная тема',
@@ -21,6 +27,12 @@ module.exports = {
     cezve: 'Джезва/Турка',
     coldbrew: 'Cold brew',
     chemex: 'Кемекс',
-    rollup: 'Свернуть раздел'
+    rollup: 'Свернуть раздел',
+    page_about: {
+      title: 'Try brew - Это open source проект созданный любителями кофе для получения новых вкусовых эмоций.',
+      works_on: 'Работает на',
+      source_code: 'Source code',
+      suggestions: 'Пожелания и предложения можно отправить на:',
+    }
   },
 };
