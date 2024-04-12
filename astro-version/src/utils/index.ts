792ebaabc55edf11bce33db7a4b8de7c0ba5410f
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2024-04-12 22:54:41 +0300

    Sort methods

--- astro-version/src/utils/index.ts
+++ astro-version/src/utils/index.ts
@@ -6,6 +6,23 @@ type RecipesByMethod = Record<string, CollectionEntry<'recipes'>[]>
 
 export type GetRecipesByMethodReturn = [string, CollectionEntry<'recipes'>[]][]
 
+const methodsOrder = new Map([
+  ['aeropress', { order: 1 }],
+  ['cezve', { order: 3 }],
+  ['chemex', { order: 2 }],
+  ['coldbrew', { order: 4 }],
+  ['pourover', { order: 0 }],
+  ['summer', { order: 5 }]
+])
+
+const sortMethods = (methons: GetRecipesByMethodReturn) => {
+  return methons.sort((a, b) => {
+    const orderA = methodsOrder.get(a[0])?.order ?? methons.length - 1
+    const orderB = methodsOrder.get(b[0])?.order ?? methons.length - 1
+    return orderA - orderB
+  })
+}
+
 export function extractMethod (slug: string) {
   // Folders inside “recipes” folder mean method
   return slug.split('/')[0]
@@ -22,5 +39,5 @@ export async function getRecipesByMethod (): Promise<GetRecipesByMethodReturn> {
     return methods
   }, {})
 
-  return Object.entries(recipesByMethod)
+  return sortMethods(Object.entries(recipesByMethod))
 }
