6ebb8e74e6f13a49631c6628b5f51edf8a4b5607
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2024-04-20 22:00:28 +0300

    Fix site

--- astro-version/astro.config.mjs
+++ astro-version/astro.config.mjs
@@ -2,7 +2,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-const isDev = import.meta.env.DEV_VERCEL
+const isDev = process.env.DEV_VERCEL
 
 export default defineConfig({
   devToolbar: {
