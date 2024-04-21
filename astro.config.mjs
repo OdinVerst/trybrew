52210fc23e07ebb998843688e18e056412041201
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2024-04-21 21:46:04 +0300

    cleanup astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-const isDev = process.env.DEV_VERCEL
 
 export default defineConfig({
   devToolbar: {
@@ -18,5 +17,5 @@ export default defineConfig({
   },
   integrations: [icon(), mdx(), sitemap()],
   output: 'static',
-  site: isDev ? undefined : 'https://trybrew.app'
+  site: 'https://trybrew.app'
 })
