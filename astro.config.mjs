bcf8c030d8e797fa71c5111c153eddc6167654e7
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2024-04-21 22:51:00 +0300

    Draft 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
+import vercel from "@astrojs/vercel/static"
 
 export default defineConfig({
   devToolbar: {
@@ -17,5 +18,13 @@ export default defineConfig({
   },
   integrations: [icon(), mdx(), sitemap()],
   output: 'static',
-  site: 'https://trybrew.app'
+  site: 'https://trybrew.app',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: false,
+    },
+    speedInsights: {
+      enabled: false,
+    },
+  }),
 })
