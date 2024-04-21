3d22dd36c7cb742d671f506da32a1d83c159a984
AleksandrSl <sl.aleksandr28@gmail.com>
2024-04-21 22:40:28 +0200

    feat: convert barn-01.md to a new format

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,15 @@ export async function getCollectionByLocale (
   })
   return recipes
 }
+
+export function humanTimeToSeconds (time: string) {
+  const coefficients = [1, 60, 24 * 60]
+  return time.split(':').map(Number).reverse().reduce((acc, cur, index) => acc + cur * coefficients[index], 0)
+}
+
+export function secondsTimeToHuman (time: number) {
+  const minutes = Math.floor((time % 3600) / 60)
+  const seconds = Math.floor((time % 60))
+
+  return minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0')
+}
